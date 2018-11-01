b585137b042fbde16e48c7c042ff0a0e24be6e89
vFISHv <513450035@qq.com>
2018-11-01 17:03:15 +0800

    build

--- example/src/main/java/com/r0adkll/slidr/example/ViewerActivity.java
+++ example/src/main/java/com/r0adkll/slidr/example/ViewerActivity.java
@@ -2,7 +2,6 @@ package com.r0adkll.slidr.example;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
